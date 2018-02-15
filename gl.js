e93ce372a7055eb750ac96a53331195228f59626
comfykernel <comfykernel@gmail.com>
2018-02-14 18:04:08 -0800

    Added buffer and shader objects as well as made rendering work

--- gl.js
+++ gl.js
@@ -1,4 +1,4 @@
-var console = (function(o_console, d_element) {
+var __console = (function(o_console, d_element) {
     return {
 	log   : function(message, color) {
 	    o_console.log(message);
@@ -63,8 +63,8 @@ var console = (function(o_console, d_element) {
     };
 }(window.console, document.getElementById("dev-console")));
 
-window.console = console;
-
+window.console = __console;
+ 
 var gelly =  { // OpenGL Puns are the best
     setCanvas: function(id) {
 	this.obj = document.getElementById(id);
@@ -76,6 +76,15 @@ var gelly =  { // OpenGL Puns are the best
 	    return false;
 	}
 
+	this.ARRAY_BUFFER    = this.gl.ARRAY_BUFFER;
+	this.ELEMENT_BUFFER  = this.gl.ELEMENT_ARRAY_BUFFER;
+
+	this.STATIC_DRAW     = this.gl.STATIC_DRAW;
+	this.DYNAMIC_DRAW    = this.gl.DYNAMIC_DRAW;
+
+	this.VERTEX_SHADER   = this.gl.VERTEX_SHADER;
+	this.FRAGMENT_SHADER = this.gl.FRAGMENT_SHADER;
+
 	return true;
     },
 
@@ -85,5 +94,137 @@ var gelly =  { // OpenGL Puns are the best
 
     clear: function() {
 	this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+    },
+
+    newBuffer: function(data, data_type, buffer_type, draw_type) {	
+	var _out = {
+	    name       : gelly.gl.createBuffer(),
+
+	    dataType   : data_type,
+	    bufferType : buffer_type,
+	    drawType   : draw_type,
+
+	    bind          : function() {
+		gelly.gl.bindBuffer(this.bufferType, this.name);
+	    },
+
+	    destroy       : function() {
+		gelly.gl.deleteBuffer(this.name);
+
+		delete this.name;
+		
+		delete this.dataType;
+		delete this.bufferType;
+		delete this.drawType;
+
+		delete this.bind;
+		delete this.bufferData;
+		delete this.bufferSubData;
+	    },
+
+	    bufferData    : function(b_data) {
+		this.bind();
+		gelly.gl.bufferData(this.bufferType,
+				    new this.dataType(b_data), this.drawType);
+	    },
+
+	    bufferSubData : function(b_s_data, offset) {
+		this.bind();
+		gelly.gl.bufferSubData(this.bufferType, offset,
+				       new this.dataType(b_s_data));
+	    }
+	};
+
+	_out.bufferData(data);
+
+	return _out;
+    },
+
+    newShader: function(data, s_type) {
+	var _out = {
+	    name : gelly.gl.createShader(s_type),
+
+	    shaderType : s_type,
+
+	    source: function(text) {
+		gelly.gl.shaderSource(this.name, text);
+	    },
+
+	    compile: function() {
+		gelly.gl.compileShader(this.name);
+
+		if(!gelly.gl.getShaderParameter(this.name, gelly.gl.COMPILE_STATUS)) {
+		    console.warn("Failed to compile shader!");
+		    console.info(gelly.gl.getShaderInfoLog(this.name));
+		}
+	    },
+
+	    destroy: function() {
+		gelly.gl.deleteShader(this.name);
+
+		delete this.name
+		
+		delete this.shaderType;
+
+		delete this.source;
+		delete this.compile;
+	    }
+	};
+
+	_out.source (data);
+	_out.compile();
+
+	return _out;
+    },
+
+    newProgram: function(shaders) {
+	var _out = {
+	    name : gelly.gl.createProgram(),
+
+	    attach: function(shaders) {
+		if(!shaders.length) {
+		    gelly.gl.attachShader(this.name, shaders.name);
+		    return;
+		}
+
+		for(var i=0; i<shaders.length; ++i) {
+		    gelly.gl.attachShader(this.name, shaders[i].name);
+		}
+	    },
+
+	    link: function() {
+		gelly.gl.linkProgram(this.name);
+
+		if(!gelly.gl.getProgramParameter(this.name, gelly.gl.LINK_STATUS)) {
+		    console.warn("Failed to link shader program!");
+		    console.info(gelly.gl.getProgramInfoLog(this.name));
+		}
+	    },
+
+	    use: function() {
+		gelly.gl.useProgram(this.name);
+	    }
+	};
+
+	_out.attach(shaders);
+	_out.link  ();
+	
+	return _out;
+    },
+
+    enableArrays: function(arrays) {
+	for(var i=0; i<arrays.length; ++i) {
+	    this.gl.enableVertexAttribArray(arrays[i]);
+	}
+    },
+
+    disableArrays: function(arrays) {
+	for(var i=0; i<arrays.length; ++i) {
+	    this.gl.disableVertexAttribArray(arrays[i]);
+	}
+    },
+
+    makeGlobal: function() {
+	gl = this.gl;
     }
 };
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
-console.log(" ___________    ___     ___    ___________    ___________    ___________"              );
+console.log("");
+console.log("     ___________    ___     ___    ___________    ___________    ___________"          );
 console.log("    /  _____   /\\  /  /\\   /  /\\  /  _____   /\\  /___   ____/\\  /  _____   /\\"   );
 console.log("   /  /\\___/  / / /  / /  /  / / /  /\\___/ _/ /  \\__/  /\\___\\/ /  /\\___/  / /"   );
 console.log("  /  / /  /  / / /  / /  /  / / /  _____  /\\/      /  / /     /  / /  /  / /"         );
@@ -11,5 +12,51 @@ console.log(" >> By Jacob Langevin >> comfykernel@gmail.com >>", "cyan");
 
 gelly.setCanvas("game-window");
 
+gelly.makeGlobal;
+
 gelly.clearColor(0.5, 0, 0.5);
 gelly.clear();
+
+var vbuff = gelly.newBuffer([-1.0, -1.0, 0.0,
+			      1.0, -1.0, 0.0,
+			      0.0,  1.0, 0.0 ],
+			    Float32Array,
+			    gelly.ARRAY_BUFFER,
+			    gelly.STATIC_DRAW);
+
+var ibuff = gelly.newBuffer([0, 1, 2],
+			    Uint16Array,
+			    gelly.ELEMENT_BUFFER,
+			    gelly.STATIC_DRAW);
+
+var vshad = gelly.newShader("\
+#version 300 es\n\
+\n\
+layout(location = 0) in vec3 pos;\n\
+                    out vec2 uv; \n\
+\n\
+void main() {\n\
+  gl_Position = vec4(pos, 1.0);\n\
+  uv          = pos.xy;        \n\
+}", gelly.VERTEX_SHADER);
+
+var fshad = gelly.newShader("\
+#version 300 es\n\
+\n\
+precision mediump float;\n\
+\n\
+in  vec2 uv;  \n\
+out vec4 frag;\n\
+\n\
+void main() {\n\
+  frag=vec4(uv, 1.0, 1.0);\n\
+}", gelly.FRAGMENT_SHADER);
+
+var prog = gelly.newProgram([vshad, fshad]);
+prog.use();
+
+gelly.enableArrays([0]);
+
+gelly.gl.vertexAttribPointer(0, 3, gelly.gl.FLOAT, false, 0, 0);
+
+gelly.gl.drawElements(gelly.gl.TRIANGLES, 3, gelly.gl.UNSIGNED_SHORT, 0);
